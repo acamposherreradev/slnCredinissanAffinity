74c33d995546d641c0e5e9aecc61478116420989
Rodrigo Cerda <rodrigo_cerda@hotmail.com>
2024-04-30 12:33:30 -0400

    se incorpora texto a summary ROD

--- Credinissan.Affinity.Web/Models/Cotizacion.cs
+++ Credinissan.Affinity.Web/Models/Cotizacion.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 
 /// <summary>
-/// Credinissan.Affinity.Web.Models
+/// Credinissan.Affinity.Web.Models ROD
 /// </summary>
 namespace Credinissan.Affinity.Web.Models
 {
