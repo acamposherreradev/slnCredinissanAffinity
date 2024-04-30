86f25ac73f7549f8592ed83ad49d195bc6a6e496
Rodrigo Cerda <rodrigo_cerda@hotmail.com>
2024-04-30 12:37:26 -0400

    hice un cambio

--- Credinissan.Affinity.Web/Models/Cotizacion.cs
+++ Credinissan.Affinity.Web/Models/Cotizacion.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 
 /// <summary>
-/// Credinissan.Affinity.Web.Models ROD
+/// Credinissan.Affinity.Web.Models ROD acampos
 /// </summary>
 namespace Credinissan.Affinity.Web.Models
 {
